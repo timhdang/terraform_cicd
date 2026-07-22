ada396c2758dce71ea739798660a2f4d4b1fb8ad
timhdang <71614530+timhdang@users.noreply.github.com>
2026-07-21 19:59:11 -0700

    Update vnet.tf

--- via_service_principal/vnet.tf
+++ via_service_principal/vnet.tf
@@ -1,10 +1,10 @@
 data "azurerm_resource_group" "dev_rg" {
-  name     = "dev-rg"
+  name = "dev-rg"
 }
 
 resource "azurerm_virtual_network" "dev_vnet" {
   name                = "dev-vnet"
-  location            = "westus2"
+  location            = data.azurerm_resource_group.dev_rg.location
   resource_group_name = data.azurerm_resource_group.dev_rg.name
   address_space       = ["10.69.0.0/24"]
 }
