e4765ad58d94e72ddc19465004aea6088cf99057
timhdang <71614530+timhdang@users.noreply.github.com>
2026-07-21 19:45:39 -0700

    Create vnet.tf

---
+++ via_service_principal/vnet.tf
@@ -0,0 +1,10 @@
+data "azurerm_resource_group" "dev_rg" {
+  name     = "dev-rg"
+}
+
+resource "azurerm_virtual_network" "dev_vnet" {
+  name                = "dev-vnet"
+  location            = "westus2"
+  resource_group_name = data.azurerm_resource_group.dev_rg.name
+  address_space       = ["10.69.0.0/24"]
+}
