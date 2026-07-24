72497365d61b87400c8e8294c34484ed3e918fb0
timhdang <71614530+timhdang@users.noreply.github.com>
2026-07-23 21:25:16 -0700

    Update vnet.tf

--- via_service_principal/vnet.tf
+++ via_service_principal/vnet.tf
@@ -8,3 +8,10 @@ resource "azurerm_virtual_network" "dev_vnet" {
   resource_group_name = data.azurerm_resource_group.dev_rg.name
   address_space       = ["10.69.0.0/24"]
 }
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = data.azurerm_resource_group.dev_rg.name
+  virtual_network_name = azurerm_virtual_network.dev_vnet.name
+  address_prefixes     = ["10.69.0.200/28"]
+}
